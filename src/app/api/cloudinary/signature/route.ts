d8b670e038d5b50ec32b3bf59185807a60a2013a
Joshua Johnson <psppay@gmail.com>
2025-07-19 16:49:18 +0000

    we are closer but we are missing custom_coordinates and source

--- src/app/api/cloudinary/signature/route.ts
+++ src/app/api/cloudinary/signature/route.ts
@@ -64,7 +64,7 @@ export async function POST(request: Request) {
     });
     
     const signature = crypto
-      .createHash('sha1')
+      .createHash('sha256')
       .update(stringToSign)
       .digest('hex');
     
